dfd51b2446b174b794f56f3611bc672c6485f590
Imre Toth <tothimre@designtech.hu>
2019-05-15 23:41:31 +0200

    DateController init

---
+++ app/Http/Controllers/DateController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DateController extends Controller
+{
+    /**
+     * Index
+     */
+    public function index() {
+        //
+    }
+}
