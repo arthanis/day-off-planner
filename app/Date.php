ceb00796a228fe9fc6ab835626eb0aee4873e691
Imre Toth <tothimre@designtech.hu>
2019-05-15 23:18:02 +0200

    Add fillable column(s)

--- app/Date.php
+++ app/Date.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Date extends Model
 {
-    //
+    /**
+     * Fillable columns
+     * @var array
+     */
+    protected $fillable = [
+        'date'
+    ];
 }
